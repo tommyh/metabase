2f41b12df364a3f69583046d96d19685c091edd5
Gustavo Saiani <gustavo@poe.ma>
2022-10-10 13:52:07 -0300

    Rely on instance for dataset value

--- frontend/src/metabase-lib/lib/Question.ts
+++ frontend/src/metabase-lib/lib/Question.ts
@@ -946,7 +946,6 @@ class QuestionInner {
     query,
     includeDisplayIsLocked,
     creationType,
-    dataset,
     ...options
   }: {
     originalQuestion?: Question;
@@ -954,7 +953,6 @@ class QuestionInner {
     query?: Record<string, any>;
     includeDisplayIsLocked?: boolean;
     creationType?: string;
-    dataset?: boolean;
   } = {}): string {
     const question = this.omitTransientCardIds();
 
@@ -967,7 +965,6 @@ class QuestionInner {
           clean,
           includeDisplayIsLocked,
           creationType,
-          dataset,
         }),
         query,
       });
@@ -1264,7 +1261,6 @@ class QuestionInner {
     clean = true,
     includeDisplayIsLocked = false,
     creationType,
-    dataset,
   } = {}) {
     const query = clean ? this.query().clean() : this.query();
     const cardCopy = {
